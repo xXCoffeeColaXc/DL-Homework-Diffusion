07d9a66647afef018cbc821d9f0b02f5064c5a2d
xXCoffeeColaXc <tamsyandro@gmail.com>
2023-10-22 17:29:42 +0200

    augmentations added

--- dataloader.py
+++ dataloader.py
@@ -75,17 +75,22 @@ class CUB200Dataset(Dataset):
 
         return image, label, bbox
     
+    def denormalize():
+        pass
+    
 
 if __name__ == "__main__":
 
     transform = transforms.Compose([
         transforms.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
+
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2),
+
         transforms.ToTensor(),
-        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # idk 
-        
         transforms.Normalize(0.5, 0.5)
     ])
-
 
     dataset = CUB200Dataset(config.ROOT_DIR, transform)
     loader = DataLoader(dataset, batch_size=5)
