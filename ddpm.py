3c586a376d7c279e8d777901b4361e3d027e3806
xXCoffeeColaXc <tamsyandro@gmail.com>
2023-11-17 21:08:40 +0100

    added build, save, restore model and logging

--- ddpm.py
+++ ddpm.py
@@ -1,25 +1,47 @@
 import torch
 import torch.nn as nn
 from tqdm import tqdm
-import config
 from utils import *
-from dataloader import get_data
-from torchvision.utils import save_image
-from modules import UNet
 from torch import optim
 import os
+from modules import UNet
+import wandb
+import time
+import datetime
+
 
 class Diffusion:
-    def __init__(self):
+    def __init__(self, config, dataloader):
+
+        self.config = config
+        self.dataloader = dataloader
+
+        self.build_model()
+
+        if self.config.wandb:
+            self.setup_logger()
+
+    def build_model(self):
+        '''Create Unet'''
+        self.unet = UNet(self.config.c_in, self.config.c_out, 
+                         self.config.conv_dim, self.config.block_depth, self.config.time_emb_dim)
+        
         # Compute alpha, beta, alpha_hat
-        self.beta = self.prepare_noise_schedule().to(config.DEVICE)
+        self.beta = self.prepare_noise_schedule().to(self.config.device)
         self.alpha = 1.0 - self.beta
         self.alpha_hat = torch.cumprod(self.alpha, dim=0)
+
+        self.opt = optim.AdamW(self.unet.parameters(), lr=self.config.lr)
+        self.mse = nn.MSELoss()
+        
+        self.print_network()
+        
+        self.unet = self.unet.to(self.config.device)
 
 
     # TODO Implement CosineScheduler
     def prepare_noise_schedule(self):
-        return torch.linspace(config.BETA_START, config.BETA_END, config.NOISE_STEPS)
+        return torch.linspace(self.config.beta_start, self.config.beta_end, self.config.noise_steps)
     
     # Add t step noise to an image / noise_images
     # x(t) = sqrt(alpha_hat)*x(0) + sqrt(1-alpha_hat)*epsilon
@@ -31,17 +53,17 @@ class Diffusion:
     
     # sample a random timestep
     def sample_timesteps(self, n):
-        return torch.randint(low=1, high=config.NOISE_STEPS, size=(n,))
+        return torch.randint(low=1, high=self.config.noise_steps, size=(n,))
 
     # NOTE Algorithm 2 Sampling from original paper
-    def sample(self, model, n):
+    def sample(self, n):
         print(f"Sampling {n} new images...") # TODO logger
-        model.eval()
+        self.unet.eval()
         with torch.no_grad():
-            x = torch.randn((n, 3, config.IMAGE_SIZE, config.IMAGE_SIZE)).to(config.DEVICE)
-            for i in tqdm(reversed(range(1, config.NOISE_STEPS)), position=0):
-                t = (torch.ones(n) * i).long().to(config.DEVICE) # create a tensor of lenght n with the current timestep
-                predicted_noise = model(x, t)
+            x = torch.randn((n, 3, self.config.image_size, self.config.image_size)).to(self.config.device)
+            for i in tqdm(reversed(range(1, self.config.noise_steps)), position=0):
+                t = (torch.ones(n) * i).long().to(self.config.device) # create a tensor of lenght n with the current timestep
+                predicted_noise = self.unet(x, t)
                 alpha = self.alpha[t][:, None, None, None]
                 alpha_hat = self.alpha_hat[t][:, None, None, None]
                 beta = self.beta[t][:, None, None, None]
@@ -50,64 +72,105 @@ class Diffusion:
                 else:
                     noise = torch.zeros_like(x)
                 x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
-        model.train()
+        self.unet.train()
         # bringing the values back to valid pixel range
         x = (x.clamp(-1, 1) + 1) / 2
         x = (x * 255).type(torch.uint8)
         return x
 
     # NOTE Algorithm 1 Traning from original paper
-    def train(self, model, opt, mse, dataloader):
-        #setup_logging(args.run_name) TODO logger
-        #logger = SummaryWriter(os.path.join("runs", args.run_name))
-        l = len(dataloader)
+    def train(self):
 
-        for epoch in range(1, config.NUM_EPOCHS):
-            #logging.info(f"Starting epoch {epoch}:") TODO logger
+        # NOTE resume training is still in progress
+        # Start training from scratch or resume training.
+        # start_epoch = 1
+        # if self.config.resume_iters:
+        #     start_epoch = self.config.resume_iters
+        #     self.model.restore_model(self.config.resume_iters) 
+
+        l = len(self.dataloader)
+
+        for epoch in range(0, self.config.epochs):
             print(f"Starting epoch {epoch}:")
-            pbar = tqdm(dataloader)
-            for i, (images, label, _) in enumerate(pbar):
-                images = images.to(config.DEVICE)
-                t = self.sample_timesteps(images.shape[0]).to(config.DEVICE) # get batch amount random timesteps
+            start_time = time.time() # duration for one epoch
+            
+            for batch_idx, (images, label, _) in enumerate(self.dataloader):
+
+                images = images.to(self.config.device)
+                t = self.sample_timesteps(images.shape[0]).to(self.config.device) # get batch amount random timesteps
                 x_t, noise = self.forward_process(images, t) # add t timestep noise to the image
-                predicted_noise = model(x_t, t) # backward process: predict the added noise
-                loss = mse(noise, predicted_noise) # calculate loss
+                predicted_noise = self.unet(x_t, t) # backward process: predict the added noise
+                mse_loss = self.mse(noise, predicted_noise) # calculate loss
 
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
+                self.opt.zero_grad()
+                mse_loss.backward()
+                self.opt.step()
 
-                pbar.set_postfix(MSE=loss.item())
-                #logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i) TODO logger
-            print(f"Epoch: {epoch} MSE: {loss.item()}")
+                loss = {}
+                loss['MSE/loss'] = mse_loss
 
-            if epoch % config.SAMPLE_STEP == 0:
-                sampled_images = self.sample(model, n=images.shape[0])
-                # TODO save_image from torch
-                # TODO save_model
-                # TODO load_model
-                save_images(sampled_images, os.path.join(config.EVAL, config.RUN_NAME, f"{epoch}.jpg"))
-                #torch.save(model.state_dict(), os.path.join("models", config.RUN_NAME, f"ckpt.pt"))
+                num_iter = batch_idx + epoch * len(self.dataloader) + 1
 
+                # Print out training information.
+                if num_iter % self.config.log_step == 0:
+                    et = time.time() - start_time
+                    et = str(datetime.timedelta(seconds=et))[:-7]
+                    log = "Elapsed [{}], Iteration [{}/{}]".format(et, batch_idx+1, len(self.dataloader))
+                    for tag, value in loss.items():
+                        log += ", {}: {:.4f}".format(tag, value)
+                    print(log)
 
-if __name__ == '__main__':
-    model = UNet().to(config.DEVICE)
+                # Sample images and save them
+                if num_iter % self.config.sample_step == 0:
+                    sampled_images = self.sample(n=images.shape[0])
+                    save_images(sampled_images, os.path.join(self.config.sample_dir, f"{num_iter}.jpg"))  # TODO save_image from torch
 
-    # TODO load model
-    # ckpt = torch.load("./working/orig/ckpt.pt")
-    # model.load_state_dict(ckpt)
-    
-    diffusion = Diffusion()
-    dataloader = get_data(type='train')
-    optimizer = optim.AdamW(model.parameters(), lr=config.LEARNING_RATE)
-    mse = nn.MSELoss()
+                # Save model
+                if num_iter % self.config.model_save_step == 0:
+                    self.save_model(num_iter)
 
-    diffusion.train(model=model, opt=optimizer, mse=mse, dataloader=dataloader)
+                # Log to wandb
+                if self.config.wandb:
+                    wandb.log({
+                        "loss": mse_loss,
+                        "epochs": epoch,
+                        })
+      
+    def test(self):
+        pass
 
-    # x = diffusion.sample(model, 2)
-    # print(x.shape)
-    # plt.figure(figsize=(32, 32))
-    # plt.imshow(torch.cat([
-    #     torch.cat([i for i in x.cpu()], dim=-1),
-    # ], dim=-2).permute(1, 2, 0).cpu())
-    # plt.show()
+    def save_model(self, num_iter):
+        unet_path = os.path.join(self.config.model_save_dir, '{}-unet.ckpt'.format(num_iter))
+        torch.save(self.unet.state_dict(), unet_path)
+        print('Saved config checkpoints into {}...'.format(self.config.model_save_dir))
+
+    def print_network(self):
+        """Print out the network information."""
+        num_params = 0
+        for p in self.unet.parameters():
+            num_params += p.numel()
+        print(self.unet)
+        print("The number of parameters: {}".format(num_params))
+
+    # NOTE 5000-unet.ckpt so the resume_iters is 5000
+    def restore_model(self, resume_iters):
+        """Restore the trained diffusion model (Unet)."""
+        print('Loading the trained model from epoch {}...'.format(resume_iters))
+        unet_path = os.path.join(self.config.model_save_dir, '{}-unet.ckpt'.format(resume_iters))
+        self.unet.load_state_dict(torch.load(unet_path, map_location=lambda storage, loc: storage))
+
+    def setup_logger(self):
+        # Initialize WandB
+        wandb.init(project='bird-diffusion-project', config={
+            "image_size": self.config.image_size,
+            "block_depth": self.config.block_depth,
+            "time_emb_dim": self.config.time_emb_dim,
+            "batch_size": self.config.batch_size,
+            "epochs": self.config.epochs,
+            "lr": self.config.lr,
+            "noise_steps": self.config.noise_steps,
+            # ... Add other hyperparameters here
+        })
+
+        # Ensure DEVICE is tracked in WandB
+        wandb.config.update({"device": self.config.device})
