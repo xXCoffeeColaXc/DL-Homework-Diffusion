fix of scaling

dataloader.py
@@ -92,11 +92,13 @@ class CUB200Dataset(Dataset):
 
 def get_data(image_dir, metadata_dir, image_size=64, batch_size=8, mode="train", num_workers=1):
     # Common transformations
+    mean = torch.tensor([0.4865, 0.4998, 0.4323])
+    std = torch.tensor([0.2326, 0.2276, 0.2659])
     base_transforms = [
         transforms.Resize(image_size, transforms.InterpolationMode.BILINEAR),
         transforms.RandomCrop(image_size),
         transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        transforms.Normalize(mean=mean, std=std),
     ]
 
     if mode == "train":

ddpm.py
@@ -79,8 +79,15 @@ class Diffusion:
                 x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
         self.unet.train()
         # bringing the values back to valid pixel range
-        x = (x.clamp(-1, 1) + 1) / 2
-        x = (x * 255).type(torch.uint8)
+        # x = (x.clamp(-1, 1) + 1) / 2
+        # x = (x * 255).type(torch.uint8)
+
+        mean = torch.tensor([0.4865, 0.4998, 0.4323])
+        std = torch.tensor([0.2326, 0.2276, 0.2659])
+
+        x = x * std + mean
+        x = x * 255
+        x = x.clamp(0, 255)
         return x
 
     # NOTE Algorithm 1 Traning from original paper